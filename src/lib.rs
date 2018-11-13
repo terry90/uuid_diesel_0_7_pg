4f5e06f3e7490e0ce1d26d81c1a5fcf471da61fa
Terry Raimondo <terry.raimondo@gmail.com>
2018-11-13 17:02:47 +0100

    Add from

--- src/lib.rs
+++ src/lib.rs
@@ -14,16 +14,22 @@ use uuid;
 #[sql_type = "UuidDiesel"]
 pub struct Uuid(pub uuid::Uuid);
 
-impl ToSql<Uuid, Pg> for Uuid {
+impl ToSql<UuidDiesel, Pg> for Uuid {
     fn to_sql<W: Write>(&self, out: &mut Output<W, Pg>) -> serialize::Result {
         out.write_all(self.0.as_bytes())?;
         Ok(IsNull::No)
     }
 }
 
-impl FromSql<Uuid, Pg> for Uuid {
+impl FromSql<UuidDiesel, Pg> for Uuid {
     fn from_sql(bytes: Option<&[u8]>) -> deserialize::Result<Self> {
         let bytes = not_none!(bytes);
         Ok(Uuid(uuid::Uuid::from_slice(bytes)?))
+    }
+}
+
+impl From<uuid::Uuid> for Uuid {
+    fn from(uuid: uuid::Uuid) -> Self {
+        Uuid(uuid)
     }
 }
